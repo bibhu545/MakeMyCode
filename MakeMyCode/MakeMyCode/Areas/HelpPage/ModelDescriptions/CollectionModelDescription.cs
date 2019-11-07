ae9162b9ad1b5c6f1f68b429840aad6756da51c4
bibhu545 <bibhuranjan.545@gmail.com>
2019-11-07 16:50:56 +0530

    Deleting WebAPI

--- MakeMyCode/MakeMyCode/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MakeMyCode.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
